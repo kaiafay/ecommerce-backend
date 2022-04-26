dd6761256aaa1319ed266b067fa1b6cd9b370e66
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-26 15:31:32 -0700

    create route to find all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,19 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// find all categories
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    // include its associated Products
+    include: [Product]
+  })
+  .then(categories => res.json(categories))
+  .catch(err => res.status(500).json(err));
 });
 
+// find one category by its `id` value
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+
 });
 
 router.post('/', (req, res) => {
