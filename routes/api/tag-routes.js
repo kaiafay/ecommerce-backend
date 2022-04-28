58c8c99dd392b32114cf72a4ba153d5751a0b171
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-27 17:26:41 -0700

    finish tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -60,8 +60,20 @@ router.put('/:id', (req, res) => {
   .catch(err => res.status(500).json(err));
 });
 
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tag => {
+    if(!tag) {
+      res.status(404).json({ message: 'No existing tag with this given ID.' })
+    }
+    res.json(tag);
+  })
+  .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
