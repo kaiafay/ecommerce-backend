5999e1a4a16a957079be90337e5a81be41a4ae67
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-27 17:41:10 -0700

    create route to delete products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,8 +109,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// delete one product by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(product => {
+    if(!product) {
+      res.status(404).json({ message: 'No existing product with this given ID.' })
+    }
+    res.json(product);
+  })
+  .catch(err => res.status(500).json(err)); 
 });
 
 module.exports = router;
