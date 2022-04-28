39b2213b7ba5a747d3f2650a66c5f6064c4ef304
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-27 17:31:17 -0700

    create route to find all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    // include category and tag data
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(products => res.json(products))
+  .catch(err => res.status(500).json(err));
 });
 
 // get one product
